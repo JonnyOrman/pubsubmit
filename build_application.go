e91a791da6ffc6a6df671ca8d5d2389cd68eaad3
Jonathan Orman <jonathanpcorman@gmail.com>
2023-09-18 12:38:48 +0100

    Use cors before setting routes

--- build_application.go
+++ build_application.go
@@ -2,6 +2,7 @@ package pubsubmit
 
 import (
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/jonnyorman/fireworks"
 )
 
@@ -24,17 +25,21 @@ func BuildApplication[T any](allowedOrigins []string) *fireworks.Application {
 		dataPublisher,
 	)
 
-	routerBuilder := fireworks.NewGinRouterBuilder()
+	// routerBuilder := fireworks.NewGinRouterBuilder()
 
-	routerBuilder.AddPost("/", requestHandler.Handle)
+	// routerBuilder.AddPost("/", requestHandler.Handle)
 
-	router := routerBuilder.Build()
+	// router := routerBuilder.Build()
+
+	router := gin.Default()
 
 	// corsConfig := cors.DefaultConfig()
 	// corsConfig.AllowOrigins = allowedOrigins
 	// router.Use(cors.New(corsConfig))
 
 	router.Use(cors.Default())
+
+	router.POST("/", requestHandler.Handle)
 
 	application := fireworks.NewApplication(router)
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	cloud.google.com/go/pubsub v1.28.0
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
 	github.com/jonnyorman/fireworks v0.1.1
 	github.com/stretchr/testify v1.8.2
@@ -17,7 +18,6 @@ require (
 	github.com/bytedance/sonic v1.8.3 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
